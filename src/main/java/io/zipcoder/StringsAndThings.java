5aca6b1fea624be6479f549bc7dcdc3de8f8e5ff
Deepti Mishra <dmishra@Zipcoders-MBP.fios-router.home>
2024-10-30 23:08:18 -0400

    completed code

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -15,7 +15,30 @@ public class StringsAndThings {
      *           countYZ("day fyyyz"); // Should return 2
      */
     public Integer countYZ(String input){
-        return null;
+
+        String [] splitString = input.split(" ");
+        int count = 0;
+        for(int i=0;i<=splitString.length-1;i++){
+
+         //   String input = "fez day";
+        System.out.println("Splitstring : " + splitString[i]);
+        int len = splitString[i].length()-1;
+        System.out.println("len : " + len);
+        String str = splitString[i];
+        System.out.println("str : " + str);
+           char extractedChar = str.charAt(len);
+            System.out.println("extracted char : " + extractedChar);
+          // count = 0;
+       if(extractedChar == 'y'  || extractedChar == 'z'){
+           count ++;
+
+            }
+            System.out.println("count : " + count);
+          //  return count;
+        }
+        System.out.println("count : " + count);
+        return count;
+
     }
 
     /**
@@ -28,7 +51,9 @@ public class StringsAndThings {
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
     public String removeString(String base, String remove){
-        return null;
+
+        return base.replace(remove,"");
+
     }
 
     /**
@@ -40,8 +65,23 @@ public class StringsAndThings {
      *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
     public Boolean containsEqualNumberOfIsAndNot(String input){
-        return null;
+        int isCount = 0;
+        int notCount = 0;
+
+        String lowercaseStr = input.toLowerCase();
+
+        for (int i = 0; i < lowercaseStr.length() - 2; i++) {
+            if (lowercaseStr.substring(i, i +2).equals("is")) {
+                isCount++;
+            } else if (lowercaseStr.substring(i, i + 3).equals("not")) {
+                notCount++;
+            }
+        }
+
+        return isCount == notCount;
     }
+
+   // }
 
     /**
      * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
@@ -51,7 +91,32 @@ public class StringsAndThings {
      *           gHappy("xxggyygxx") // Should return  false
      */
     public Boolean gIsHappy(String input){
-        return null;
+//
+	 //All g's are happy
+
+        int len = input.length();
+        boolean happy = true;
+
+        for (int i = 0; i < len - 1; i++) {
+            if (input.charAt(i) == 'g') {
+                //first
+                if (i > 0 && input.charAt(i - 1) == 'g') {
+                    happy = true;
+                    //anything in between
+                } else if (input.charAt(i - 1) == 'g' || input.charAt(i + 1) == 'g') {
+                    happy = true;
+                    //last
+                } else if (i < len - 1 && input.charAt(i + 1) == 'g') {
+                    happy = true;
+                } else
+
+                    happy = false;
+            }
+
+        }
+
+        return happy;
+
     }
 
 
@@ -63,6 +128,21 @@ public class StringsAndThings {
      *            countTriple("a") // Should return 0
      */
     public Integer countTriple(String input){
-        return null;
+        int l = input.length(); // Get the length of the given string
+        int ctr = 0; // Initialize a counter for triples
+
+        // Loop through the string to check for triples
+        for (int i = 0; i < l - 2; i++) {
+            char tmp = input.charAt(i); // Get the character at index 'i'
+
+            // Check if the character at index 'i' is the same as the next two characters
+            if (tmp == input.charAt(i + 1) && tmp == input.charAt(i + 2)) {
+                ctr++; // Increment the counter if a triple is found
+            }
+        }
+        return ctr; // Return the total count of triples
     }
-}
+
+        //return null;
+    }
+
